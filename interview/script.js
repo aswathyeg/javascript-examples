f0b77a75c17c90074ceb780af77690a62c1157e6
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-10-25 23:50:00 +0530

    map and filter examples

--- interview/script.js
+++ interview/script.js
@@ -10,3 +10,30 @@ function test() {
   console.log(a);
 }
 test();
+// hoisting
+function abc() {
+  console.log(a);
+  //   const c = 30;
+  //     let b = 20;
+  var a = 10;
+}
+abc();
+
+//map
+var a = [1, 2, 3, 4];
+function mapping() {
+  var multiply = a.map((prod) => {
+    return prod * 5;
+  });
+  console.log(multiply);
+}
+mapping();
+//filter
+var a = [1, 2, 3, 4];
+function fil() {
+  var reduceArray = a.filter((c) => {
+    return c > 2;
+  });
+  console.log(reduceArray);
+}
+fil();
